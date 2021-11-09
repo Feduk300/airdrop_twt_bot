4ba2354858cf49249be2a57e142cb55b3d655b2a
shece <lolkald2@gmail.com>
2021-11-10 01:53:44 +0700

    1

--- handlers/users/names.py
+++ handlers/users/names.py
@@ -8,8 +8,7 @@ from loader import dp, RegistrationStates, UsersDb
 @dp.message_handler(commands="admcommand")
 async def admins(message: types.Message):
     names = await UsersDb.users(message.chat.id)
-    user = names[0][3]
-    number_card= names[0][4]
+
     text="Пользователи, которые заплатили:"
     for tab in names:
         text += f"\n {tab[4]}"
