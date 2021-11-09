85a17be3052686ba2ace691bebfd85310ff7fd18
shece <lolkald2@gmail.com>
2021-11-10 01:59:47 +0700

    1

--- handlers/users/party.py
+++ handlers/users/party.py
@@ -6,4 +6,4 @@ from loader import dp
 
 @dp.message_handler(text="Ближайшая туса")
 async def info(message: types.Message):
-    await message.answer("*02.01.2022**17:00**-**03.01.2022**13:00*")
+    await message.answer("*02.01.2022 17:00 - 03.01.2022 13:00*", parse_mode= "Markdown")
