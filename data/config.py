b0b8c6004f1d1a9e0bc8fc8ff680d0caaf9d209c
shece <lolkald2@gmail.com>
2021-11-22 15:22:05 +0700

    easy

--- data/config.py
+++ data/config.py
@@ -1,10 +1,10 @@
-BOT_TOKEN = "2048816333:AAGtwSGPHnLzCs5hV-iEo-8ageYfJYAm6ng"
+BOT_TOKEN = "1979602120:AAHqwPbURkB_4YDdWI6YyQdmTZ3U-5-n4zk"
 
 check_channel = [-1001520775425]
 
-amount = 1100
+amount = 1800
 
 POSTGRES = {
     "host": "postgresql://postgres@localhost/twt_bot",
-    "password": "brff21" #brff21
+    "password": "" #brff21
 }
